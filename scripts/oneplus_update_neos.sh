cb93a76641eb7526c0bda84dc83f690469f98c49
eFini <ricklan@gmail.com>
2021-12-10 14:17:13 +1000

    fix invalid var name

--- scripts/oneplus_update_neos.sh
+++ scripts/oneplus_update_neos.sh
@@ -6,7 +6,7 @@ fi
 
 source "$BASEDIR/launch_env.sh"
 echo "Installing NEOS update"
-NEOS_PY="$DIR/selfdrive/hardware/eon/neos.py"
-MANIFEST="$DIR/selfdrive/hardware/eon/oneplus.json"
+NEOS_PY="$BASEDIR/selfdrive/hardware/eon/neos.py"
+MANIFEST="$BASEDIR/selfdrive/hardware/eon/oneplus.json"
 $NEOS_PY --swap-if-ready $MANIFEST
-$DIR/selfdrive/hardware/eon/updater $NEOS_PY $MANIFEST
+$BASEDIR/selfdrive/hardware/eon/updater $NEOS_PY $MANIFEST
