4f2257a2044729fae1483e8c85bba72c00b1f922
wer5lcy <33412324+wer5lcy@users.noreply.github.com>
2022-01-01 15:38:07 +0800

    Switch to my accel profile and 0812 model

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -43,10 +43,10 @@ DP_ACCEL_NORMAL = 1
 DP_ACCEL_SPORT = 2
 
 # accel profile by @arne182 modified by @wer5lcy
-_DP_CRUISE_MIN_V = [-2.0, -1.8, -1.6, -1.4, -1.2]
-_DP_CRUISE_MIN_V_ECO = [-2.0, -1.6, -1.4, -1.2, -1.0]
-_DP_CRUISE_MIN_V_SPORT = [-3.0, -2.6, -2.3, -2.0, -1.0]
-_DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 55.0]
+_DP_CRUISE_MIN_V = [-5.0, -3.5, -3.5]
+_DP_CRUISE_MIN_V_ECO = [-5.0, -3.5, -3.5]
+_DP_CRUISE_MIN_V_SPORT = [-5.0, -3.5, -3.5]
+_DP_CRUISE_MIN_BP = [0.0, 5.0, 55.0]
 
 _DP_CRUISE_MAX_V = [1.6, 1.4, 1.0, 0.6, 0.3]
 _DP_CRUISE_MAX_V_ECO = [1.5, 1.3, 0.8, 0.4, 0.2]
